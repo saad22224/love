a543bec12efc026b7f581fef6e6622d8adc367f1
saad22224 <152297632+saad22224@users.noreply.github.com>
2024-03-25 23:20:15 +0200

    Add files via upload

---
+++ js/saad.js
@@ -0,0 +1,7 @@
+let love = document.querySelector(".ri-heart-3-fill")
+  let div = document.querySelector("div")
+
+   love.onclick = function () {
+    love.style.color = "#fff"
+    love.textContent = "You Are My heart (انت قلبي)"
+   }
